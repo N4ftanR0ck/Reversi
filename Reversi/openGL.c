fddbf8ad56eb715b5443901d6ecf411172bec720
Mariana <Mariana@DESKTOP-0U8DRKN>
2024-06-07 07:30:11 +0300

    oops...

--- Reversi/openGL.c
+++ Reversi/openGL.c
@@ -60,10 +60,10 @@ int main(void)
 	{
 		for (int j = 0; j < 8; j++)
 		{
-			board[i][j] = 1;
+			board[i][j] = 0;
 		}
 	}
-	board[3][4] = 0;
+	board[3][4] = 1;
 	board[4][3] = 1;
 	board[4][4] = -1;
 	board[3][3] = -1;
